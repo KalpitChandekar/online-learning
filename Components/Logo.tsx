d1dc962bbcedaa3e2094b60a2aedcad9d4dcd186
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-22 11:12:21 +0530

    create logo

---
+++ Components/Logo.tsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <div className="relative flex items-center">
+      <div className="h-12 w-12 rounded-md rotate-45 bg-[#65DAFF] -z-10"></div>
+      <h1 className="text-[#113C49] text-3xl font-bold absolute left-1">
+        Shilline
+      </h1>
+    </div>
+  );
+};
+export default Logo;
