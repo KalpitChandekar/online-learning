98c3a026b1b61145448107ddc7ae1a20de6f4cab
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-24 10:01:17 +0530

    update logo

--- Components/Logo.tsx
+++ Components/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <div className="relative flex items-center">
+    <div className="relative flex items-center z-10">
       <div className="h-12 w-12 rounded-md rotate-45 bg-[#65DAFF] -z-10"></div>
       <h1 className="text-[#113C49] text-3xl font-bold absolute left-1">
         Shilline
