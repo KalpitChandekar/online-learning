2241aebaace27d477db81735a5b99660d34cefc8
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-29 20:44:04 +0530

    [feat]: Add News & Resources Section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Companies from "@/Components/Companies";
 import Hero from "@/Components/Hero";
 import Integration from "@/Components/Intergration";
 import Navbar from "@/Components/Navbar";
+import NewsResources from "@/Components/NewsResources";
 import OurFeatures from "@/Components/OurFeatures/page";
 import Specification from "@/Components/Specification";
 import Testimonial from "@/Components/Testimonial";
@@ -20,6 +21,7 @@ const page = () => {
       <OurFeatures />
       <Integration />
       <Testimonial />
+      <NewsResources />
     </div>
   );
 };
