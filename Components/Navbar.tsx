31a61e6d4632829f0f5a56a37bd7fb239c005054
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-30 12:14:48 +0530

    make navbar responsive

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,22 +2,22 @@ import Logo from "./Logo";
 
 const Navbar = () => {
   return (
-    <div className="max-w-5xl mx-auto z-10 pt-6 flex justify-between">
+    <div className="max-w-5xl px-4 md:px-0 mx-auto z-10 pt-6 flex justify-between">
       <a href="/">
         <Logo />
       </a>
 
-      <ul className="flex text-[#252641] items-center gap-10">
-        <li>
+      <ul className="flex text-[#252641] items-center gap-2 md:gap-10">
+        <li className="hidden md:block">
           <a href="/">Home</a>
         </li>
-        <li>
+        <li className="hidden md:block">
           <a href="/">Blog</a>
         </li>
-        <li>
+        <li className="hidden md:block">
           <a href="/">Careers</a>
         </li>
-        <li>
+        <li className="hidden md:block">
           <a href="/">About Us</a>
         </li>
         <button className="bg-white text-[#6C6C6C] rounded-full px-5 py-1">
