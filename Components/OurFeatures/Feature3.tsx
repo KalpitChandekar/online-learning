bf2234056603c2a5c67bd41f3db8267cb4d8fa08
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-30 12:24:45 +0530

    make third feature responsive

--- Components/OurFeatures/Feature3.tsx
+++ Components/OurFeatures/Feature3.tsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 
 const Feature3 = () => {
   return (
-    <div className="flex gap-20 items-center">
-      <div className="relative w-[30rem]">
+    <div className="flex md:flex-row flex-col-reverse gap-20 items-center">
+      <div className="relative md:w-[30rem]">
         <Image src="/feature3.png" alt="feature1" width={624} height={724} />
       </div>
 
       <div className="space-y-6 max-w-sm">
         <h1 className="text-2xl font-bold text-blue">
-          Assessments,<br /> <span className="text-orange">Quizzes,</span> Tests
+          Assessments,
+          <br /> <span className="text-orange">Quizzes,</span> Tests
         </h1>
         <p className="text-gray">
           Easily launch live assignments, quizzes, and tests. Student results
