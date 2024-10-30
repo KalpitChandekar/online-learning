5dcb99598fbb4495b9c36ebf32bba907ff80ae3e
Kalpit <kalpitchandekar1736@gmail.com>
2024-10-30 12:15:54 +0530

    make companies responsive

--- Components/Companies.tsx
+++ Components/Companies.tsx
@@ -2,15 +2,51 @@ import Image from "next/image";
 
 const Companies = () => {
   return (
-    <div className="max-w-5xl mx-auto space-y-8 text-center my-40">
+    <div className="max-w-5xl mx-auto px-4 md:px-0 space-y-8 text-center my-40">
       <p className="text-gray">Trusted by 5,000+ Companies Worldwide</p>
-      <div className="flex justify-between">
-        <Image src="/google.png" alt="Companies" width={151} height={45}  className="w-24"/>
-        <Image src="/netflix.png" alt="Companies" width={151} height={45}  className="w-24"/>
-        <Image src="/airbnb.png" alt="Companies" width={151} height={45}  className="w-24"/>
-        <Image src="/amazon.png" alt="Companies" width={151} height={45} className="w-24" />
-        <Image src="/facebook.png" alt="Companies" width={151} height={45}  className="w-24"/>
-        <Image src="/grab.png" alt="Companies" width={151} height={45}  className="w-24"/>
+      <div className="md:flex md:justify-between grid grid-cols-3 gap-6 md:gap-0 items-center">
+        <Image
+          src="/google.png"
+          alt="Companies"
+          width={151}
+          height={45}
+          className="w-24"
+        />
+        <Image
+          src="/netflix.png"
+          alt="Companies"
+          width={151}
+          height={45}
+          className="w-24"
+        />
+        <Image
+          src="/airbnb.png"
+          alt="Companies"
+          width={151}
+          height={45}
+          className="w-24"
+        />
+        <Image
+          src="/amazon.png"
+          alt="Companies"
+          width={151}
+          height={45}
+          className="w-24"
+        />
+        <Image
+          src="/facebook.png"
+          alt="Companies"
+          width={151}
+          height={45}
+          className="w-24"
+        />
+        <Image
+          src="/grab.png"
+          alt="Companies"
+          width={151}
+          height={45}
+          className="w-24"
+        />
       </div>
     </div>
   );
